57622f4f9b263973aa24471c7061737723b1057d
angel <angelsol299@hotmail.com>
2019-04-15 19:55:37 +0200

    deleting a todo

--- arrays/main.js
+++ arrays/main.js
@@ -19,3 +19,11 @@ addForm.addEventListener("submit", e => {
     addForm.reset();
   }
 });
+
+// delete todos
+
+list.addEventListener("click", e => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
