087fc8b8950eb85e9ffeaa15b0307edb6df4c08a
angel <angelsol299@hotmail.com>
2019-04-15 19:46:26 +0200

    adding the todos

--- arrays/main.js
+++ arrays/main.js
@@ -1,7 +1,16 @@
 const addForm = document.querySelector(".add");
 
+const generateTemplate = todo => {
+  const html = `
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${todo}</span>
+    <i class="far fa-trash-alt delete"></i>
+  </li>
+  `;
+};
+
 addForm.addEventListener("submit", e => {
   e.preventDefault();
   const todo = addForm.add.value.trim();
-  console.log(todo);
+  generateTemplate(todo);
 });
