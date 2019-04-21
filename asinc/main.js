bee28df8e3dbf1e8291a6eb3cfc74d03684369df
angel <angelsol299@hotmail.com>
2019-04-21 19:58:54 +0200

    fetch api

--- asinc/main.js
+++ asinc/main.js
@@ -5,7 +5,11 @@ const getTodos = async () => {
   const data = await response.json();
   return data;
 };
+console.log(1);
+console.log(2);
 getTodos().then(data => console.log("resolve:", data));
+console.log(3);
+console.log(4);
 
 // fetch("todos/bolo.json")
 //   .then(response => {
