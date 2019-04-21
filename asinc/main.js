b68fe388ce3a84bc55abc0d914b4ce23b0a51fa0
angel <angelsol299@hotmail.com>
2019-04-21 19:49:12 +0200

    fetch api

--- asinc/main.js
+++ asinc/main.js
@@ -3,6 +3,10 @@
 fetch("todos/bolo.json")
   .then(response => {
     console.log("resolved", response);
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
   })
   .catch(err => {
     console.log("rejected", err);
