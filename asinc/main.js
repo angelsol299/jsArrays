948ffc088e7484ccff7305807ef0ba79bb773123
angel <angelsol299@hotmail.com>
2019-04-21 19:52:49 +0200

    fetch api

--- asinc/main.js
+++ asinc/main.js
@@ -1,16 +1,22 @@
 //fetch api
 
-fetch("todos/bolo.json")
-  .then(response => {
-    console.log("resolved", response);
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(err => {
-    console.log("rejected", err);
-  });
+const getTodos = async () => {
+  const response = await fetch("todos/luigi.json");
+};
+const test = getTodos();
+console.log(test);
+
+// fetch("todos/bolo.json")
+//   .then(response => {
+//     console.log("resolved", response);
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log("rejected", err);
+//   });
 
 //chaining promises
 
